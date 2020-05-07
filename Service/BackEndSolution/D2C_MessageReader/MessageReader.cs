f460242bb38dd928d7b387c25e412aa03182af75
Sanjay Kumar <SanjayKumar.Bahadur@Ltts.com>
2020-05-07 10:02:22 +0530

    Message reader added

---
+++ Service/BackEndSolution/D2C_MessageReader/MessageReader.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.Azure.EventHubs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace D2C_MessageReader
+{
+    class MessageReader
+    {      // Event Hub-compatible endpoint
+        // az iot hub show --query properties.eventHubEndpoints.events.endpoint --name {your IoT Hub name}
+        private readonly static string _eventHubsCompatibleEndpoint = Environment.GetEnvironmentVariable("EventHubCompatibleEndpoint");
+
+        // Event Hub-compatible name
+        // az iot hub show --query properties.eventHubEndpoints.events.path --name {your IoT Hub name}
+        private readonly static string _eventHubsCompatiblePath = Environment.GetEnvironmentVariable("EventHubCompatiblePath");
+
+        // az iot hub policy show --name service --query primaryKey --hub-name {your IoT Hub name}
+        private readonly static string _iotHubSasKey = Environment.GetEnvironmentVariable("IoTHubSasKey");
+        private readonly static string _iotHubSasKeyName = Environment.GetEnvironmentVariable("IoTHubSasKeyName");
+
+        private static EventHubClient _eventHubClient;
+        private System.Timers.Timer timer;
+        public  async void InvokeMethod()
+        {
+            await CreateEventHubClientAsync();
+
+            // Create a timer and set a two second interval.
+            timer = new System.Timers.Timer();
+            timer.Interval = 1000;
+
+            // Hook up the Elapsed event for the timer. 
+            timer.Elapsed += OnTimedEvent;
+
+            // Have the timer fire repeated events (true is the default)
+            timer.AutoReset = true;
+
+            // Start the timer
+            timer.Enabled = true;
+
+            // Create a PartitionReciever for each partition on the hub.
+            var runtimeInfo = _eventHubClient.GetRuntimeInformationAsync().GetAwaiter().GetResult();
+            var d2cPartitions = runtimeInfo.PartitionIds;
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+
+            Console.CancelKeyPress += (s, e) =>
+            {
+                e.Cancel = true;
+                cts.Cancel();
+                Console.WriteLine("Exiting...");
+            };
+
+            var tasks = new List<Task>();
+            foreach (string partition in d2cPartitions)
+            {
+                tasks.Add(ReceiveMessagesFromDeviceAsync(partition, cts.Token));
+            }
+
+            // Wait for all the PartitionReceivers to finsih.
+            Task.WaitAll(tasks.ToArray());
+
+
+        }
+        private static void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            Console.WriteLine("The Elapsed event was raised at {0}", e.SignalTime);
+        }
+        private Task CreateEventHubClientAsync()
+        {
+            // Create an EventHubClient instance to connect to the
+            // IoT Hub Event Hubs-compatible endpoint.
+            var connectionString = new EventHubsConnectionStringBuilder(new Uri(_eventHubsCompatibleEndpoint), _eventHubsCompatiblePath, _iotHubSasKeyName, _iotHubSasKey);
+            _eventHubClient = EventHubClient.CreateFromConnectionString(connectionString.ToString());
+
+            return Task.CompletedTask;
+        }
+
+        // Asynchronously create a PartitionReceiver for a partition and then start 
+        // reading any messages sent from the simulated client.
+        private static async Task ReceiveMessagesFromDeviceAsync(string partition, CancellationToken ct)
+        {
+            // Create the receiver using the default consumer group.
+            // For the purposes of this sample, read only messages sent since 
+            // the time the receiver is created. Typically, you don't want to skip any messages.
+            var eventHubReceiver = _eventHubClient.CreateReceiver("$Default", partition, EventPosition.FromEnqueuedTime(DateTime.Now));
+            Console.WriteLine("Create receiver on partition: " + partition);
+            while (true)
+            {
+                if (ct.IsCancellationRequested) break;
+                Console.WriteLine("Listening for messages on: " + partition);
+                // Check for EventData - this methods times out if there is nothing to retrieve.
+                var events = await eventHubReceiver.ReceiveAsync(100);
+
+                // If there is data in the batch, process it.
+                if (events == null) continue;
+
+                foreach (EventData eventData in events)
+                {
+                    string data = Encoding.UTF8.GetString(eventData.Body.Array);
+                    Console.WriteLine("Message received on partition {0}:", partition);
+                    Console.WriteLine("  {0}:", data);
+                    Console.WriteLine("Application properties (set by device):");
+                    foreach (var prop in eventData.Properties)
+                    {
+                        Console.WriteLine("  {0}: {1}", prop.Key, prop.Value);
+                    }
+                    Console.WriteLine("System properties (set by IoT Hub):");
+                    foreach (var prop in eventData.SystemProperties)
+                    {
+                        Console.WriteLine("  {0}: {1}", prop.Key, prop.Value);
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ Service/BackEndSolution/D2C_MessageReader/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Azure.EventHubs;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace D2C_MessageReader
+{
+    class Program
+    {
+  
+        static void Main(string[] args)
+        {
+            Console.WriteLine("IoT Hub: Read device to cloud messages. Ctrl-C to exit.\n");
+
+            MessageReader reader = new MessageReader();
+            reader.InvokeMethod();
+        }
+    }
+}
